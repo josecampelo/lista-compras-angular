9d3472f55976de2e6a1fe4b2b28574780e3f8734
José Campelo <josecampelojr@gmail.com>
2026-01-01 08:09:39 -0300

    feat: cria lógica de adicionar item a lista de compras

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ItemLista } from '../models/item-lista.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  listaItens: ItemLista[] = [];
+  input: string = '';
+  
+  adicionaItem() {
+    let item: ItemLista = {
+      id: this.listaItens.length + 1,
+      nome: this.input,
+      comprado: false
+    };
 
+    if (this.input.trim()) {
+      this.listaItens.push(item);
+    }
+
+    this.input = '';
+  }
 }
